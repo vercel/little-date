77d84e8c552e24f2f0c1023f4dbabfa85d9d263c
Timo Lins <me@timo.sh>
2023-11-16 18:12:28 +0100

    Add more cases

--- src/format-date-range.test.ts
+++ src/format-date-range.test.ts
@@ -1,67 +1,153 @@
-import { expect, test } from "vitest";
+import { expect, describe, test } from "vitest";
+import type { DateRangeFormatOptions } from "./format-date-range";
 import { formatDateRange } from "./format-date-range";
 
-const today = new Date("2021-08-01T12:00:00.000Z");
+const today = new Date("2023-11-15T12:00:00.000Z");
 
-test("format date range same month", () => {
-  const from = new Date("2021-01-01T12:00:00.000Z");
-  const to = new Date("2021-01-12T12:00:00.000Z");
+const defaultOptions: DateRangeFormatOptions = {
+  today,
+  locale: "en-US",
+};
 
-  expect(formatDateRange(from, to, today)).toBe("Jan 1 - 12");
-});
+describe("format date range", () => {
+  test("format date range same month", () => {
+    const from = new Date("2023-01-01T00:00:00.000Z");
+    const to = new Date("2023-01-12T23:59:59.999Z");
 
-test("format date range different month", () => {
-  const from = new Date("2021-01-03T12:00:00.000Z");
-  const to = new Date("2021-04-20T12:00:00.000Z");
+    expect(formatDateRange(from, to, defaultOptions)).toBe("Jan 1 - 12");
+  });
 
-  expect(formatDateRange(from, to, today)).toBe("Jan 3 - Apr 20");
-});
+  test("format date range different month", () => {
+    const from = new Date("2023-01-03T00:00:00.000Z");
+    const to = new Date("2023-04-20T23:59:59.999Z");
 
-test("format date range different year", () => {
-  const from = new Date("2020-01-01T12:00:00.000Z");
-  const to = new Date("2021-01-20T12:00:00.000Z");
+    expect(formatDateRange(from, to, defaultOptions)).toBe("Jan 3 - Apr 20");
+  });
 
-  expect(formatDateRange(from, to, today)).toBe("Jan 1, '20 - Jan 20, '21");
-});
+  test("format date range different year", () => {
+    const from = new Date("2022-01-01T00:00:00.000Z");
+    const to = new Date("2023-01-20T23:59:59.999Z");
 
-test("format date range full day", () => {
-  const from = new Date("2021-01-01T00:00:00.000Z");
-  const to = new Date("2021-01-01T23:59:59.999Z");
+    expect(formatDateRange(from, to, defaultOptions)).toBe(
+      "Jan 1 '22 - Jan 20 '23"
+    );
+  });
 
-  expect(formatDateRange(from, to, today)).toBe("Fri, Jan 1");
-});
+  test("format date range full day", () => {
+    const from = new Date("2023-01-01T00:00:00.000Z");
+    const to = new Date("2023-01-01T23:59:59.999Z");
 
-test("format date range full day different year", () => {
-  const from = new Date("2020-01-01T00:00:00.000Z");
-  const to = new Date("2020-01-01T23:59:59.999Z");
+    expect(formatDateRange(from, to, defaultOptions)).toBe("Sun, Jan 1");
+  });
 
-  expect(formatDateRange(from, to, today)).toBe("Wed, Jan 1, 2020");
-});
+  test("format date range same day, different hours", () => {
+    const from = new Date("2023-01-01T00:11:00.000Z");
+    const to = new Date("2023-01-01T14:30:59.999Z");
 
-test("format date range full month", () => {
-  const from = new Date("2021-04-01T00:00:00.000Z");
-  const to = new Date("2021-04-30T23:59:59.999Z");
+    expect(formatDateRange(from, to, defaultOptions)).toBe(
+      "Jan 1, 12:11am - 2:30pm"
+    );
+  });
 
-  expect(formatDateRange(from, to, today)).toBe("April 2021");
-});
+  test("format date range same day, different hours, with 24-hour locale", () => {
+    const from = new Date("2023-01-01T00:11:00.000Z");
+    const to = new Date("2023-01-01T14:30:59.999Z");
 
-test("format date range full month different year", () => {
-  const from = new Date("2020-01-01T00:00:00.000Z");
-  const to = new Date("2020-01-31T23:59:59.999Z");
+    expect(
+      formatDateRange(from, to, {
+        ...defaultOptions,
+        locale: "en-GB",
+      })
+    ).toBe("Jan 1, 0:11 - 14:30");
+  });
 
-  expect(formatDateRange(from, to, today)).toBe("January 2020");
-});
+  test("format date range different days with time", () => {
+    const from = new Date("2023-01-01T00:11:00.000Z");
+    const to = new Date("2023-01-02T14:30:00.000Z");
 
-test("format date range hour difference", () => {
-  const from = new Date("2021-01-01T12:00:00.000Z");
-  const to = new Date("2021-01-01T12:59:59.999Z");
+    expect(formatDateRange(from, to, defaultOptions)).toBe(
+      "Jan 1, 12:11am - Jan 2, 2:30pm"
+    );
+  });
 
-  expect(formatDateRange(from, to, today)).toBe("Jan 1, 12:00pm - 12:59pm");
-});
+  test("format date range different days with single time", () => {
+    const from = new Date("2023-01-01T00:11:00.000Z");
+    const to = new Date("2023-01-02T23:59:59.999Z");
 
-test("format date range full year", () => {
-  const from = new Date("2021-01-01T00:00:00.000Z");
-  const to = new Date("2021-12-31T23:59:59.999Z");
+    expect(formatDateRange(from, to, defaultOptions)).toBe(
+      "Jan 1, 12:11am - Jan 2"
+    );
+  });
 
-  expect(formatDateRange(from, to, today)).toBe("2021");
+  test("format date range different years with time", () => {
+    const from = new Date("2022-01-01T00:11:00.000Z");
+    const to = new Date("2023-01-02T14:30:00.000Z");
+
+    expect(formatDateRange(from, to, defaultOptions)).toBe(
+      "Jan 1 '22, 12:11am - Jan 2 '23, 2:30pm"
+    );
+  });
+
+  test("format date range different years with time, includeTime: false", () => {
+    const from = new Date("2022-01-01T00:11:00.000Z");
+    const to = new Date("2023-01-02T14:30:00.000Z");
+
+    expect(
+      formatDateRange(from, to, {
+        ...defaultOptions,
+        includeTime: false,
+      })
+    ).toBe("Jan 1 '22 - Jan 2 '23");
+  });
+
+  test("format date range full day different year", () => {
+    const from = new Date("2022-01-01T00:00:00.000Z");
+    const to = new Date("2022-01-01T23:59:59.999Z");
+
+    expect(formatDateRange(from, to, defaultOptions)).toBe("Sat, Jan 1, 2022");
+  });
+
+  test("format date range full month", () => {
+    const from = new Date("2023-04-01T00:00:00.000Z");
+    const to = new Date("2023-04-30T23:59:59.999Z");
+
+    expect(formatDateRange(from, to, defaultOptions)).toBe("April 2023");
+  });
+
+  test("format date range full month different year", () => {
+    const from = new Date("2023-01-01T00:00:00.000Z");
+    const to = new Date("2023-01-31T23:59:59.999Z");
+
+    expect(formatDateRange(from, to, defaultOptions)).toBe("January 2023");
+  });
+
+  test("format date range hour difference", () => {
+    const from = new Date("2023-01-01T12:00:00.000Z");
+    const to = new Date("2023-01-01T12:59:59.999Z");
+
+    expect(formatDateRange(from, to, defaultOptions)).toBe(
+      "Jan 1, 12pm - 12:59pm"
+    );
+  });
+
+  test("format date range today, different hours", () => {
+    const from = today;
+    const to = new Date(today.getTime() + 60 * 60 * 1000); // Today + 1 hour
+
+    expect(formatDateRange(from, to, defaultOptions)).toBe("12pm - 1pm");
+  });
+
+  test("format date range full year", () => {
+    const from = new Date("2023-01-01T00:00:00.000Z");
+    const to = new Date("2023-12-31T23:59:59.999Z");
+
+    expect(formatDateRange(from, to, defaultOptions)).toBe("2023");
+  });
+
+  test("format date range quarter", () => {
+    const from = new Date("2023-01-01T00:00:00.000Z");
+    const to = new Date("2023-03-31T23:59:59.999Z");
+
+    expect(formatDateRange(from, to, defaultOptions)).toBe("Q1 2023");
+  });
 });
--- src/format-date-range.ts
+++ src/format-date-range.ts
@@ -10,64 +10,73 @@ import {
   endOfDay,
   isSameMinute,
   startOfYear,
+  getQuarter,
+  startOfQuarter,
+  endOfQuarter,
   endOfYear,
 } from "date-fns";
 
-export const replaceAmPm = (text: string) =>
-  (text || "").replace(/ AM/g, "am").replace(/ PM/g, "pm");
+const shortenAmPm = (text: string): string => {
+  const shortened = (text || "").replace(/ AM/g, "am").replace(/ PM/g, "pm");
+  const withoutDoubleZero = shortened.replace(/:00/g, "");
+  return withoutDoubleZero;
+};
 
-export const formatTime = (date?: Date) =>
-  replaceAmPm(
-    date?.toLocaleTimeString(undefined, {
-      hour: "2-digit",
-      minute: "2-digit",
-    }) || ""
-  );
+const removeLeadingZero = (text: string): string => text.replace(/^0/, "");
 
-const getNavigatorLanguage = () => {
+export const createFormatTime =
+  (locale?: string) =>
+  (date: Date): string =>
+    removeLeadingZero(
+      shortenAmPm(
+        date.toLocaleTimeString(locale, {
+          hour: "2-digit",
+          minute: "2-digit",
+        }) || ""
+      )
+    );
+
+const getNavigatorLanguage = (): string => {
   if (typeof window === "undefined") {
     return "en-US";
   }
   return window.navigator.language;
 };
 
-export const formatDate = (
-  date: Date,
-  interval: "day" | "hour" | "minute" | "month"
-) => {
-  const lang = getNavigatorLanguage();
-  switch (interval) {
-    case "day":
-      return (
-        date?.toLocaleDateString(lang, {
-          day: "2-digit",
-        }) + "."
-      );
-    case "hour":
-      const loc = date?.toLocaleTimeString(lang, {
-        hour: "numeric",
-      });
-      return replaceAmPm(loc);
-    case "minute":
-      return formatTime(date);
-    case "month":
-      return date?.toLocaleDateString(lang, {
-        month: "short",
-      });
-  }
-};
+export interface DateRangeFormatOptions {
+  today?: Date;
+  locale?: string;
+  includeTime?: boolean;
+}
 
-export const formatDateRange = (from: Date, to: Date, today = new Date()) => {
+export const formatDateRange = (
+  from: Date,
+  to: Date,
+  {
+    today = new Date(),
+    locale = getNavigatorLanguage(),
+    includeTime = true,
+  }: DateRangeFormatOptions = {}
+): string => {
   const sameYear = isSameYear(from, to);
   const sameMonth = isSameMonth(from, to);
   const sameDay = isSameDay(from, to);
   const thisYear = isSameYear(from, today);
 
-  if (!sameYear) {
-    return `${format(from, "LLL d, ''yy")} - ${format(to, "LLL d, ''yy")}`;
-  }
   const yearSuffix = thisYear ? "" : `, ${format(to, "yyyy")}`;
 
+  const formatTime = createFormatTime(locale);
+
+  const startTimeSuffix =
+    includeTime && !isSameMinute(startOfDay(from), from)
+      ? `, ${formatTime(from)}`
+      : "";
+
+  const endTimeSuffix =
+    includeTime && !isSameMinute(endOfDay(to), to) ? `, ${formatTime(to)}` : "";
+
+  // Check if the range is the entire year
+  // Example: 2023
   if (
     isSameMinute(startOfYear(from), from) &&
     isSameMinute(endOfYear(to), to)
@@ -75,10 +84,18 @@ export const formatDateRange = (from: Date, to: Date, today = new Date()) => {
     return `${format(from, "yyyy")}`;
   }
 
-  if (!sameMonth) {
-    return `${format(from, "LLL d")} - ${format(to, "LLL d")}${yearSuffix}`;
+  // Check if the range is an entire quarter
+  // Example: Q1 2023
+  if (
+    isSameMinute(startOfQuarter(from), from) &&
+    isSameMinute(endOfQuarter(to), to) &&
+    getQuarter(from) === getQuarter(to)
+  ) {
+    return `Q${getQuarter(from)} ${format(from, "yyyy")}`;
   }
 
+  // Check if the range is the entire month
+  // Example: January 2023
   if (
     isSameMinute(startOfMonth(from), from) &&
     isSameMinute(endOfMonth(to), to)
@@ -86,20 +103,68 @@ export const formatDateRange = (from: Date, to: Date, today = new Date()) => {
     return `${format(from, "LLLL yyyy")}`;
   }
 
+  // Range across years
+  // Example: Jan 1 '23 - Feb 12 '24
+  if (!sameYear) {
+    return `${format(from, "LLL d ''yy")}${startTimeSuffix} - ${format(
+      to,
+      "LLL d ''yy"
+    )}${endTimeSuffix}`;
+  }
+
+  // Range across months
+  // Example: Jan 1 - Feb 12[, 2023]
+  if (!sameMonth) {
+    return `${format(from, "LLL d")}${startTimeSuffix} - ${format(
+      to,
+      "LLL d"
+    )}${endTimeSuffix}${yearSuffix}`;
+  }
+
+  // Range across days
   if (!sameDay) {
+    // Check for a time suffix, if so print the month twice
+    // Example: Jan 1, 12:00pm - Jan 2, 1:00pm[, 2023]
+    if (startTimeSuffix || endTimeSuffix) {
+      return `${format(from, "LLL d")}${startTimeSuffix} - ${format(
+        to,
+        "LLL d"
+      )}${endTimeSuffix}${yearSuffix}`;
+    }
+
+    // Example: Jan 1 - 12[, 2023]
     return `${format(from, "LLL d")} - ${format(to, "d")}${yearSuffix}`;
   }
 
-  if (isSameMinute(startOfDay(from), from) && isSameMinute(endOfDay(to), to)) {
+  // Range across hours
+  if (isSameDay(from, to)) {
+    // With times
+    // Example: Jan 1, 12:00pm - 1:00pm[, 2023]
+    if (startTimeSuffix || endTimeSuffix) {
+      // If it's today, don't include the date
+      // Example: 12:00pm - 1:00pm
+      if (isSameDay(from, today)) {
+        return `${formatTime(from)} - ${formatTime(to)}`;
+      }
+
+      return `${format(from, "LLL d")}${startTimeSuffix} - ${formatTime(
+        to
+      )}${yearSuffix}`;
+    }
+
+    // Full day
+    // Example: Fri, Jan 1[, 2023]
     return `${format(from, "eee, LLL d")}${yearSuffix}`;
   }
 
+  // Example: Jan 1, 12:00pm - 1:00pm[, 2023]
   if (isSameHour(from, to)) {
-    return `${format(from, "LLL d")}, ${formatTime(from)} - ${formatTime(
+    return `ABV${format(from, "LLL d")}, ${formatTime(from)} - ${formatTime(
       to
     )}${yearSuffix}`;
   }
 
-  const lang = getNavigatorLanguage();
-  return `${from.toLocaleDateString(lang)} - ${to.toLocaleDateString(lang)}`;
+  return `${from.toLocaleDateString(locale)} - ${to.toLocaleDateString(
+    locale
+  )}`;
 };
